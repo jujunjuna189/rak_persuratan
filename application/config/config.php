a5cbaba2d62b385a6ec3fdc467933be950c9d6c3
Jujun Juna189 <53271616+jujunjuna189@users.noreply.github.com>
2023-05-01 23:52:32 +0700

    Change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-project/rak-persuratan/';
+$config['base_url'] = 'http://localhost/ci-project/rak_persuratan/';
 
 /*
 |--------------------------------------------------------------------------
